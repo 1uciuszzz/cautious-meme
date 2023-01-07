a622d41aa3c93300ed0610c5c680a36e0b451623
1uciuszzz <nick_zzhao@outlook.com>
2023-01-08 00:12:31 +0800

    project init as same as official-site

---
+++ packages/official-site-admin/src/pages/index.js
@@ -0,0 +1,11 @@
+import color from "../styles/color.module.scss";
+
+function HomePage() {
+  return (
+    <h1 className="text-5xl" style={{ color: color.primary }}>
+      Official Site Admin
+    </h1>
+  );
+}
+
+export default HomePage;
